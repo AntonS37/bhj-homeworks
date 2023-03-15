375835eb2ced3254c2e52f578770698d120fbb9b
Neto <5308014@mail.ru>
2023-03-16 00:37:57 +0300

    first commit

---
+++ html-forms/chat/task.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const chat = document.querySelector('.chat-widget');
+const chatContainer = document.querySelector('.chat-widget__messages-container');
+const messages = document.querySelector('.chat-widget__messages');
+const input = document.querySelector('.chat-widget__input');
+let i = 0;
+
+function scrollChat() {
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+}
+
+function answerRobot() {
+    const messagesRobot = [
+        'Вы не купили ни одного товара, чтобы так с нами разговаривать!',
+        'Кто тут?',
+        'Где ваша совесть?',
+        'Добрый день! До свидания!',
+        'Мы ничего не будем вам продавать',
+    ]
+    i = (i + 1) % messagesRobot.length;
+    let time = new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+    messages.innerHTML += `
+        <div class="message">
+            <div class="message__time">${time}</div>
+            <div class="message__text">
+                ${messagesRobot[i]}
+            </div>
+        </div>`;
+    scrollChat();
+}
+
+function chatActive(e) {
+    let value = input.value.trim();
+    if(e.key === "Enter" && value !== '') {
+        let time = new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+        messages.innerHTML += `
+            <div class="message_client">
+                <div class="message__time">${time}</div>
+                <div class="message__text">
+                    ${value}
+                </div>
+            </div>`;
+        input.value = '';
+        scrollChat();
+        setTimeout(answerRobot, 1000);
+    }
+}
+
+chat.addEventListener('click', () => {
+    chat.classList.add('chat-widget_active');
+});
+
+input.addEventListener('keydown', chatActive);
